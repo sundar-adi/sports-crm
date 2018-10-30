ca4fbdf61c39479d160a03273035de0b8a5844f4
Emilio A <emilio.sanchez@gmail.com>
2018-10-30 17:22:19 +0100

    User edition on admin, and connect user to profile public view

--- hptx/user/models.py
+++ hptx/user/models.py
@@ -1,45 +1,29 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
-from base.models import SimpleModel
 
 
 class User(AbstractUser):
 
     autocomplete_search_field = 'username'
 
-    def autocomplete_label(self):
-        return self.username
-
-
-# Create your models here.
-class Profile(SimpleModel):
-    user = models.OneToOneField(
-        verbose_name=_(u'user'),
-        to=settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
     birthday = models.DateField(
-        verbose_name=_(u'birthday'),
+        verbose_name=_(u'Birthday'),
         blank=True,
         null=True
     )
     city = models.CharField(
         max_length=255,
-        verbose_name=_(u'city'),
+        verbose_name=_(u'City'),
         blank=True,
-        null=True
     )
     state = models.CharField(
         max_length=255,
-        verbose_name=_(u'state'),
+        verbose_name=_(u'State'),
         blank=True,
-        null=True
     )
     bio = models.TextField(
-        verbose_name=_(u'bio'),
+        verbose_name=_(u'Bio'),
         blank=True,
     )
     photo = models.ForeignKey(
@@ -50,3 +34,31 @@ class Profile(SimpleModel):
         blank=True,
         on_delete=models.SET_NULL,
     )
+    twitter_handler = models.CharField(
+        max_length=255,
+        verbose_name=_('Twitter handler'),
+        blank=True,
+    )
+    instagram_handler = models.CharField(
+        max_length=255,
+        verbose_name=_('Instagram handler'),
+        blank=True,
+    )
+    facebook_handler = models.CharField(
+        max_length=255,
+        verbose_name=_('Facebook handler'),
+        blank=True,
+    )
+    rating = models.DecimalField(
+        verbose_name=_('Rating'),
+        max_digits=2,
+        decimal_places=1,
+        default=0,
+    )
+    is_talent = models.BooleanField(
+        verbose_name=_('Is talent'),
+        default=False,
+    )
+
+    def autocomplete_label(self):
+        return self.username
