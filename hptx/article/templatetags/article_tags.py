ea8c4b51f43b708047888522aabfbd9ac174d402
Alejandro Diaz Perez <3balejandro@gmail.com>
2018-10-31 13:11:46 +0100

    Podcast page menu active (WIP)

--- hptx/article/templatetags/article_tags.py
+++ hptx/article/templatetags/article_tags.py
@@ -45,3 +45,8 @@ def share(context, article):
     context['article'] = article
     context['article_url'] = article.full_url
     return context
+
+
+@register.filter
+def pdb(article):
+    import pdb; pdb.set_trace()
