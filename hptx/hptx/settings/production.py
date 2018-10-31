eaa2cc47bb2605a65552cfffd37f4067bffececa
Emilio A <emilio.sanchez@gmail.com>
2018-10-31 18:36:21 +0100

    Production settings

--- hptx/hptx/settings/production.py
+++ hptx/hptx/settings/production.py
@@ -4,6 +4,20 @@ DEBUG = False
 
 SECRET_KEY = ')&@)2lu%e)+&q!mcbyx2%1tbvsbm!xwfn9cwn5w_pgx+w2!*-+'
 
+DATABASES['default'].update({
+    'NAME': 'hptx',
+    'USER': 'dbmaster',
+    'ENGINE': 'django.db.backends.postgresql',
+    'PASSWORD': 'SgVY6W$354Y1',
+    'HOST': '',
+    'PORT': '',
+})
+
+ALLOWED_HOSTS = (
+    'hptx.us',
+    'pre.hptx.us',
+)
+
 try:
     from .local import *
 except ImportError:
