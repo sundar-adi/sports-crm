06ff209348356ceb07fb530c5800bfae43c7ba61
Emilio A <emilio.sanchez@gmail.com>
2018-11-01 19:29:32 +0100

    Talent of the week

--- hptx/user/templatetags/user_tags.py
+++ hptx/user/templatetags/user_tags.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
@@ -16,4 +19,25 @@ def rating(rating):
             stars.append('empty')
     return {
         'stars': stars
+    }
+
+
+@register.inclusion_tag('user/talent_of_the_week.html', takes_context=True)
+def talent_of_the_week(context):
+    homepage = context.get('homepage', None)
+    talent = homepage and homepage.talent_of_the_week
+    now = timezone.now()
+    today = datetime.date(now.year, now.month, now.day)
+    week_from = today - datetime.timedelta(days=today.weekday())
+    week_to = week_from + datetime.timedelta(days=6)
+    if talent:
+        last_article = talent.author_on.live().order_by(
+            '-publication_date').first()
+    else:
+        last_article = None
+    return {
+        'talent': talent,
+        'last_article': last_article,
+        'week_from': week_from,
+        'week_to': week_to,
     }
