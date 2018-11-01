7eaffe4ab7b5ba936489f65f1040f89071c4d03a
Emilio A <emilio.sanchez@gmail.com>
2018-11-01 11:26:25 +0100

    Added sentry

--- hptx/hptx/settings/production.py
+++ hptx/hptx/settings/production.py
@@ -29,6 +29,73 @@ EMAIL_HOST_PASSWORD = 'Am8JAzMctk3lP6fKPoWnP1+tjb8z48t1U6dTTlvOUqoD'
 EMAIL_PORT = 2587
 DEFAULT_FROM_EMAIL = 'no-reply@hptx.us'
 
+# RAVEN
+INSTALLED_APPS = INSTALLED_APPS + [
+    'raven.contrib.django.raven_compat', ]
+MIDDLEWARE = MIDDLEWARE + [
+    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware', ]
+
+RELEASE = "production"
+RAVEN_CONFIG = {
+    'dsn': 'https://eab68db1aad94fe7a95214987524d485@sentry.io/1313762',
+    'release': RELEASE,
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'sentry': {
+            # To capture more than ERROR, change to WARNING, INFO, etc.
+            'level': 'ERROR',
+            'class':
+                'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'hatch.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django_cron': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    }
+}
+
 try:
     from .local import *
 except ImportError:
