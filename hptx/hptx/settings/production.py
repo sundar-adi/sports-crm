16858e4b7d59de0a3aa47ca582cac760dfdf41bd
Emilio A <emilio.sanchez@gmail.com>
2018-11-01 10:54:16 +0100

    Email configuration

--- hptx/hptx/settings/production.py
+++ hptx/hptx/settings/production.py
@@ -20,6 +20,15 @@ ALLOWED_HOSTS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../static_root')
 
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'AKIAITRUEGII5PYJG5LQ'
+EMAIL_HOST_PASSWORD = 'Am8JAzMctk3lP6fKPoWnP1+tjb8z48t1U6dTTlvOUqoD'
+EMAIL_PORT = 2587
+DEFAULT_FROM_EMAIL = 'webmaster@hptx.us'
+
 try:
     from .local import *
 except ImportError:
