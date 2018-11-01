e5ecf3fa25b086703d8a73082a8712155b2839dd
dander94 <3balejandro@gmail.com>
2018-11-01 18:10:32 +0100

    Fast responsive version of the webpage

--- frontend/src/components/signupForm.vue
+++ frontend/src/components/signupForm.vue
@@ -1,55 +1,55 @@
 <template>
   <form v-on:submit.prevent="submit" novalidate>
      <div class="uk-flex uk-flex-between uk-flex-wrap uk-flex-wrap-between">
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
            <label class="uk-form-label">First Name</label>
            <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
             <input id="id_first_name" v-model="form.first_name" maxlength="30" name="first_name" required="required" type="text" class="uk-input">
             <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.first_name">{{ error }}</span>
           </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
            <label class="uk-form-label">Last Name</label>
            <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
             <input id="id_last_name"v-model="form.last_name"  maxlength="30" name="last_name" required="required" type="text" class="uk-input">
             <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.last_name">{{ error }}</span>
           </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
            <label class="uk-form-label">Username</label>
            <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
             <input id="id_username" v-model="form.username" maxlength="254" name="username" required="required" type="text" class="uk-input">
             <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.username">{{ error }}</span>
           </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
            <label class="uk-form-label">Email</label>
            <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
             <input id="id_email" v-model="form.email" maxlength="254" name="email" required="required" type="email" class="uk-input">
             <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.email">{{ error }}</span>
           </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
            <label class="uk-form-label">Password</label>
            <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
               <div class="uk-inline uk-width-expand"><span class="uk-form-icon uk-form-icon-flip"><i class="fas fa-lock"></i></span> <input id="id_password" v-model="form.password" name="password" required="required" type="password" class="uk-input"></div>
               <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.password">{{ error }}</span>
            </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
            <label class="uk-form-label"> Retype Password</label>
            <div class="uk-form-controls uk-margin-medium-bottom uk-margin">
               <div class="uk-inline uk-width-expand"><span class="uk-form-icon uk-form-icon-flip"><i class="fas fa-lock"></i></span> <input id="id_password2" v-model="form.password2" name="password2" required="required" type="password" class="uk-input"></div>
               <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.password2">{{ error }}</span>
            </div>
         </div>
-        <div class="uk-width-1-2 uk-margin-right uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-margin-right uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
            <label class="uk-form-label">Address</label>
            <div class="uk-margin-medium-bottom uk-margin-right uk-form-controls"><input v-model="form.address" name="address" placeholder="Street, Number..." type="text" class="uk-input uk-form-success">
              <span class="fs-xs fw-medium fc-brand-primary" v-for="error in errors.address">{{ error }}</span>
            </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
           <label class="uk-form-label">City</label>
           <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
            <input id="id_city" v-model="form.city" name="city" required="required" type="text" class="uk-input">
@@ -57,7 +57,7 @@
           </div>
 
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-right">
            <label class="uk-form-label">ZIP</label>
            <div class="uk-margin uk-margin-small-bottom uk-form-controls">
              <input v-model="form.zip_code" name="zip_code" type="text" class="uk-input">
@@ -65,7 +65,7 @@
            </div>
         </div>
         </div>
-        <div class="uk-width-1-2 uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
+        <div class="uk-width-1-2@m uk-padding-small uk-padding-remove-vertical uk-padding-remove-left">
           <label class="uk-form-label">State</label>
           <div class="uk-margin-medium-bottom uk-margin uk-form-controls">
            <input id="id_state" v-model="form.state" name="state" required="required" type="text" class="uk-input">
@@ -77,7 +77,7 @@
      <div>
         <p class="uk-flex"><input type="checkbox" v-model="privacy" class="uk-checkbox uk-margin-small-right uk-flex-none uk-margin-top"><span class="fs-sm">By signing up to this subscription plan and agreeing to the Privacy Policy you agree to this web site storing your information.</span></p>
     </div>
-     <input v-if="!loading" type="submit" value="proccess subscription" class="uk-button uk-button-primary uk-width-1-2 uk-margin-medium-top" :disabled="!ready" :class="{'uk-button-primary--disabled': !ready}">
+     <input v-if="!loading" type="submit" value="proccess subscription" class="uk-button uk-button-primary uk-width-1-2@m uk-margin-medium-top" :disabled="!ready" :class="{'uk-button-primary--disabled': !ready}">
      <p class="uk-margin-medium-top" v-if="loading">We're processing your information{{ this.loadingMsg }}</p>
   </form>
 </template>
