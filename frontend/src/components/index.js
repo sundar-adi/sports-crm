66cc84ae88ab037190603ba1ecbad4b8c481c192
Ana <anamarmen90@gmail.com>
2018-11-05 11:46:05 +0100

    modal paywall

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 
 import Ellipsis from './ellipsis.vue'
+import ModalPaywall from './modalPaywall.vue'
 import SignupForm from './signupForm.vue'
 import StripeButton from './stripeButton.vue'
+
 
 export default {
 	install(Vue) {
 		Vue.component('ellipsis', Ellipsis);
+		Vue.component('modalPaywall', ModalPaywall);
 		Vue.component('signupForm', SignupForm);
 	}
 };
